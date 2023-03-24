f309d60bf53b0d0e1cde7cb300886fce1140f18b
Roger <rsd.santo@outlook.com>
2023-03-24 04:03:09 -0300

    ++ feat: page creation to display all posts, pagination component creation ++ ATV-1

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import * as styles from './styles';
+import { BsChevronLeft, BsChevronRight, BsChevronDoubleLeft, BsChevronDoubleRight } from 'react-icons/bs';
+
+type PaginationType = {
+  currentPage: number;
+  handlePagination: (value: number) => void;
+  pages: number;
+};
+
+export const Pagination = ({ currentPage, handlePagination, pages }: PaginationType) => {
+  const [pagination, setPagination] = useState<number>(currentPage);
+
+  const handleChevronDoubleLeft = () => {
+    setPagination(1);
+  };
+
+  const handleChevronLeft = () => {
+    setPagination(pagination - 1);
+  };
+
+  const handleChevronRight = () => {
+    setPagination(pagination + 1);
+  };
+
+  const handleChevronDoubleRight = () => {
+    setPagination(pages);
+  };
+
+  useEffect(() => {
+    handlePagination(pagination);
+  }, []);
+
+  return (
+    <styles.Container>
+      <button onClick={handleChevronDoubleLeft} disabled={pagination === 1}>
+        <BsChevronDoubleLeft />
+      </button>
+
+      <button onClick={handleChevronLeft} disabled={pagination === 1}>
+        <BsChevronLeft />
+      </button>
+
+      <strong>
+        {pagination}/{pages}
+      </strong>
+
+      <button onClick={handleChevronRight} disabled={pages === pagination}>
+        <BsChevronRight />
+      </button>
+
+      <button onClick={handleChevronDoubleRight} disabled={pages === pagination}>
+        <BsChevronDoubleRight />
+      </button>
+    </styles.Container>
+  );
+};
---
+++ src/pages/[id].tsx
@@ -0,0 +1,9 @@
+const postID = () => {
+  return (
+    <>
+      <h2>Post ID</h2>
+    </>
+  );
+};
+
+export default postID;
