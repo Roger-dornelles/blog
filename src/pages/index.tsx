49ef8a5271f89ff10e7c5463a98e1004335f73ca
Roger <rsd.santo@outlook.com>
2023-03-24 16:34:32 -0300

    ++ feat: creation of the layout component and changing the view of the posts ++ ATV-3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,86 +1,27 @@
-import * as Styles from '@/styles/styled';
-import axios from 'axios';
-import { DataType, ResponsePropsType } from '@/types/postsAll';
-import { useState } from 'react';
+import * as styles from '@/styles/styled';
 import Head from '@/components/Header';
-import Link from 'next/link';
-import { GetServerSideProps } from 'next';
-import { Pagination } from '@/components/Pagination';
+import BackgroundSvg from '@/components/BackgroundSvg';
 
-export default function Home({ error, message, data }: ResponsePropsType) {
-  const [posts] = useState(data as DataType[] | null);
-  const [itemsPerPage] = useState<number>(9);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-
-  const pages = posts && Math.ceil(posts.length / itemsPerPage);
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const currentItems = posts && posts.slice(startIndex, endIndex);
-
-  const handlePagination = (value: number) => {
-    setCurrentPage(value);
-  };
+export default function Home() {
   return (
     <>
       <Head />
+      <styles.Container>
+        <BackgroundSvg />
 
-      <Styles.H1>Bloguinho</Styles.H1>
-      <Styles.Paragraph>Fique por dentro das noticias</Styles.Paragraph>
-      <Styles.Container>
-        {posts &&
-          currentItems?.map((posts: DataType) => {
-            return (
-              <Styles.Post key={posts.id}>
-                <Link href={`postId/${posts.id}`}>
-                  <p>{posts?.title}</p>
-                  <p>{posts?.body}</p>
-                </Link>
-              </Styles.Post>
-            );
-          })}
+        <styles.H1>Bloguinho</styles.H1>
 
-        {error && <Styles.Error>{message}</Styles.Error>}
-      </Styles.Container>
+        <styles.ContainerParagraphs>
+          <styles.Paragraph>Fique por dentro das noticias</styles.Paragraph>
+          <styles.Paragraph>Tudo o que acontece no mundo,</styles.Paragraph>
+          <styles.Paragraph>VOCÊ fica sabendo aqui.</styles.Paragraph>
+        </styles.ContainerParagraphs>
 
-      {!error && <Pagination currentPage={currentPage} handlePagination={handlePagination} pages={pages} />}
+        <styles.ContainerParagraphs marginTop={'2rem'}>
+          <styles.Paragraph>Entre e saiba o que esta acontecendo no mundo</styles.Paragraph>
+          <styles.Paragraph>Clique e saiba o que estão comentando.</styles.Paragraph>
+        </styles.ContainerParagraphs>
+      </styles.Container>
     </>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  try {
-    const posts = await axios({
-      url: `${process.env.NEXT_PUBLIC_URL}/posts`,
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!posts.data) {
-      return {
-        props: {
-          error: true,
-          message: 'Não há posts para exibir',
-          data: null,
-        },
-      };
-    }
-
-    return {
-      props: {
-        error: false,
-        message: null,
-        data: posts.data,
-      },
-    };
-  } catch (error) {
-    return {
-      props: {
-        error: true,
-        message: 'Ocorreu um erro, tente mais tarde',
-        data: null,
-      },
-    };
-  }
-};
